08e70f2b1922802f31f7df4fb194f2c557df332f
Lorena Triana Diaz <trianadiaz2020@gmail.com>
2025-04-30 16:08:18 -0500

    v 0.3

---
+++ docs/Programa.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([4.7526743,-74.1195301,17.75], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
